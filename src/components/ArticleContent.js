b6fd27f98756e0e5adbe667852483456f21734c3
hagai-tz <52886733+hagai-tz@users.noreply.github.com>
2020-01-30 11:34:13 +0200

    before changing the grid

--- src/components/ArticleContent.js
+++ src/components/ArticleContent.js
@@ -10,12 +10,13 @@ import ArticleRawData from '../stores/ArticleRawData'
 
 @observer
 class ArticleContent extends Component {
+    
     render() {
-        console.log(this.props.processedData.rawData.content)
+        console.log("I'm content", this.props.processedData.rawData.content)
 
 
         return (
-
+            
             <div id='article-content'>
                 {this.props.processedData.rawData.content.map(word => {
                         return (
@@ -38,3 +39,49 @@ export default ArticleContent;
 // style={ (this.props.processedData.difficultyLevel<50 ? { color:"red"} : {display: "none"}) }
 // style={ (this.props.processedData.difficultyLevelValue<50 ? { color:"red"} : {display: "none"}) }
 // {/* <span className='translatedWord' difficultyLevel={ word.difficultyLevel } style={ (this.props.processedData.difficultyLevelValue<word.difficultyLevel ? null: {transition: "display 2s", display: "none" }) } > {word.translatedWord}</span> */ }
+
+
+
+
+
+
+
+
+//
+// import React, { Component } from 'react';
+// import './CSS/ArticleContent.css'
+// import { Skeleton } from 'antd';
+
+
+// import { observer, action, inject } from 'mobx-react'
+// import ArticleRawData from '../stores/ArticleRawData'
+
+// @inject("processedData")
+
+// @observer
+// class ArticleContent extends Component {
+    
+//     render() {
+//         console.log(this.props.processedData.rawData.content)
+
+
+//         return (
+
+//             <div id='article-content'>
+//                 {this.props.processedData.rawData.content.map(word => {
+//                         return (
+//                             <span className='word-ul'>
+//                                 <span className="word"> {word.word} </span>
+//                                 <span className='translatedWord' difficultyLevel={ word.difficultyLevel } style={ (this.props.processedData.difficultyLevelValue>word.difficultyLevel ? null: { visibility: "hidden", transition:"visibility 1s"} )}> {word.translatedWord}</span>
+//                             </span>
+//                         )
+//                     })
+//                 }
+                
+//             </div>
+//         );
+//     }
+// }
+
+// export default ArticleContent;
+
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { Icon, Slider, Menu, Dropdown } from 'antd';
 import "antd/dist/antd.css";
 import './CSS/Nav.css'
+import mainLogo from'./logo.png';
+
 
 import { observer, action, inject } from 'mobx-react'
 import ArticleRawData from '../stores/ArticleRawData'
@@ -16,8 +18,6 @@ class Nav extends Component {
     
 
     languageHandler = (e) => {
-        console.log(e)
-        console.log(e.target.value)
         this.props.processedData.changeTranslationLanguage(e.target.value)
     }
     render() {
@@ -25,17 +25,17 @@ class Nav extends Component {
         const logo = "<p>"
         return (
             <div id='nav-bar'>
-                <span id='nav-log-top-left'>{logo}
-                </span>
+                <img id='logo' src={mainLogo} />
+                {/* <span id='nav-log-top-left'>{logo}</span> */}
                 {/* <button lang='ar' onClick={this.languageHandler} >ar</button> */}
                 <Slider vertical defaultValue={100} onChange={this.props.processedData.translationSlider} className='slider'/>
                 <span className='easy'>Easy</span>
                 <span className='hard'>Hard</span>
                 <Icon className='line' type="line" style={{ fontSize: '15px', alignItems: 'center' }} />
 
-            <select onChange={this.languageHandler} className='menu-flags'>
-                     {langs.map(lang => <option value={lang}>{lang}</option>)}
-            </select>
+                <select onChange={this.languageHandler} className='menu-flags'>
+                        {langs.map(lang => <option value={lang}>{lang}</option>)}
+                </select>
 
                 
             </div>
